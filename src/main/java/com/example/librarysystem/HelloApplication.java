0c5bf20cc8b2e522b8e24b232bf5a4b8fd2cfa40
moustafa69 <96343198+moustafa69@users.noreply.github.com>
2022-05-30 21:18:41 +0200

    new pane

--- src/main/java/com/example/librarysystem/HelloApplication.java
+++ src/main/java/com/example/librarysystem/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.librarysystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +13,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        StackPane p1 = new StackPane();
+        Label l1 = new Label("kosm kda");
+        p1.getChildren().add(l1);
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(p1, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
