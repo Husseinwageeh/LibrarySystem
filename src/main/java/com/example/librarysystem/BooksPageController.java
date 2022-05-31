eb59a907cc336fb2c1b42532c019e26eded75d01
moustafa69 <96343198+moustafa69@users.noreply.github.com>
2022-06-01 01:04:33 +0200

    made a lot of changes

--- src/main/java/com/example/librarysystem/BooksPageController.java
+++ src/main/java/com/example/librarysystem/BooksPageController.java
@@ -1,4 +1,26 @@
 package com.example.librarysystem;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class BooksPageController {
+    public AnchorPane BookPane;
+
+
+    public void OpenAddBook(ActionEvent event) throws IOException {
+        FXMLLoader root = new FXMLLoader(HelloApplication.class.getResource("AddBook.fxml"));
+        Parent OpenBooks= root.load();
+        MembersPageController b = root.getController();
+        BookPane.getChildren().removeAll();
+        BookPane.getChildren().setAll(OpenBooks);
+
+
+    }
+
+    public void RemoveBook(ActionEvent event) {
+    }
 }
