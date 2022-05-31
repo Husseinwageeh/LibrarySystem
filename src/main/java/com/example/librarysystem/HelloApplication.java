cade8bfa62473b674627af56dfc2543bcb2c5cc9
moustafa69 <96343198+moustafa69@users.noreply.github.com>
2022-05-31 12:55:22 +0200

    ay 5ara

--- src/main/java/com/example/librarysystem/HelloApplication.java
+++ src/main/java/com/example/librarysystem/HelloApplication.java
@@ -2,11 +2,13 @@ package com.example.librarysystem;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -14,9 +16,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StartUpPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        FXMLLoader root = new FXMLLoader(HelloApplication.class.getResource("StartUpPage.fxml"));
+
+        Scene scene = new Scene(root.load(), 320, 240);
+        stage.setTitle("AAST LIBRARY");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/librarysystem/StartUpPageController.java
@@ -0,0 +1,13 @@
+package com.example.librarysystem;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class StartUpPageController {
+
+
+
+
+
+}
