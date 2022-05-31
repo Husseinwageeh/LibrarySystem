44386d2d64ce541b0b4360942bed8b01f55a7dfe
moustafa69 <96343198+moustafa69@users.noreply.github.com>
2022-05-31 23:05:32 +0200

    new data base class added for database connection

---
+++ src/main/java/com/example/librarysystem/JDBC.java
@@ -0,0 +1,20 @@
+package com.example.librarysystem;
+import java.sql.*;
+public class JDBC {
+    public void test() {
+
+        try {
+            //when using awad's database use password "32484463248446"
+            //when using hussein's database use password "toor"
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/root", "root", "ps4dragon4");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from book");
+            while(resultSet.next())
+            {
+                System.out.println(resultSet.getString("name"));
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
