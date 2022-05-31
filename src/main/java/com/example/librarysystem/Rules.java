35da65bd4794018562a28653fa772264a337dbb8
Hussein Wageeh <husseinwageeh@gmail.com>
2022-05-31 20:50:58 +0200

    Rules are added.

--- src/main/java/com/example/librarysystem/Rules.java
+++ src/main/java/com/example/librarysystem/Rules.java
@@ -1,35 +1,33 @@
 package com.example.librarysystem;
 
-import javafx.css.Rule;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class Rules {
-    @FXML
-    static Label RulesLabel;
-    private Rules (String Rules)
+
+    static private Rules r;
+    public String getRules="\t\t\t\tRules for Library\n" +
+            "\t\t\t------------------------------\n" +
+            "The Ultimate Rule: لو انت بتاخد سكشن مع م/ محمد فوزي يحق لك استعارة اي كتاب في اي وقت\n" +
+            "\n" +
+            "Normal Rules: \n" +
+            "1- Keep books clean and dry \n" +
+            "2- No food or drinks near the books\n" +
+            "3- keep books away from pets and babies (3ala asas enna hnd5al pets ya kosmk)\n" +
+            "4- Do not color or draw in books\n" +
+            "5- If you want to borrow a book you must become a member first\n" +
+            "6- when you enter the library you must salute the portrait of Mohamed Fawzy\n" +
+            "7- Do not fuck with or in the library\n" +
+            "---------------------------------------------------------------------------------------";
+
+    private Rules()
     {
-        this.RulesLabel.setText(Rules);
+
     }
 
-    static Rules singleton(String Rules)
+    static Rules getInstance()
     {
-        if(Rules == null)
+        if(r == null)
         {
-            Rules rules = new Rules("\t\t\t\tRules for Library\n" +
-                    "\t\t\t------------------------------\n" +
-                    "The Ultimate Rule: لو انت بتاخد سكشن مع م/ محمد فوزي يحق لك استعارة اي كتاب في اي وقت\n" +
-                    "\n" +
-                    "Normal Rules: \n" +
-                    "1- Keep books clean and dry \n" +
-                    "2- No food or drinks near the books\n" +
-                    "3- keep books away from pets and babies (3ala asas enna hnd5al pets ya kosmk)\n" +
-                    "4- Do not color or draw in books\n" +
-                    "5- If you want to borrow a book you must become a member first\n" +
-                    "6- when you enter the library you must salute the portrait of Mohamed Fawzy\n" +
-                    "7- Do not fuck with or in the library\n" +
-                    "---------------------------------------------------------------------------------------");
+           return new Rules();
         }
-        return "Rule";
+        return r;
     }
 }
--- src/main/java/com/example/librarysystem/RulesPageController.java
+++ src/main/java/com/example/librarysystem/RulesPageController.java
@@ -1,6 +1,21 @@
 package com.example.librarysystem;
 
-public class RulesPageController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RulesPageController implements Initializable {
 
 
+    public Label RulesLabel;
+
+    @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+   Rules r= Rules.getInstance();
+   RulesLabel.setText(r.getRules);
+
+
+  }
 }
